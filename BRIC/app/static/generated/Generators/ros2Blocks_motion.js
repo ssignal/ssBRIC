3f9324cb43e4dbc7f2d5e2829203769d50f7eaf5
kiho.choi <kiho.choi@lge.com>
2026-03-24 17:59:51 +0900

    [app] Improve motion option descriptions and UI tooltips
    - Refined `BTList.json` with detailed descriptions for motion options and parameters, enhancing user experience.
    - Added hover-based tooltips in `generator_engine.py` and `bt_function.js` to display option descriptions interactively.

--- BRIC/app/static/generated/Generators/ros2Blocks_motion.js
+++ BRIC/app/static/generated/Generators/ros2Blocks_motion.js
@@ -2,63 +2,158 @@
 const javascriptGenerator = (window.javascript && window.javascript.javascriptGenerator) || window.javascriptGenerator;
 const OPTION_PARAM_MAP = {
   "behavior__motion__motion_start_motion": {
-    "PARAM_STATE": {
-      "autonomous": [
+    "PARAM_TASK_TYPE": {
+      "Expressive": [
         {
-          "name": "A1",
-          "field": "OPT_STATE_A1",
+          "name": "name",
+          "field": "OPT_TASK_TYPE_NAME",
           "type": "string",
-          "description": "String A1",
+          "description": "Motion name",
           "options": [
             [
-              "AA1",
-              "AA1"
+              "wave_two_hand",
+              "wave_two_hand"
             ],
             [
-              "AA2",
-              "AA2"
-            ]
-          ],
-          "default": "AA1",
-          "option_parameters": {}
-        },
-        {
-          "name": "A2",
-          "field": "OPT_STATE_A2",
-          "type": "integer",
-          "description": "Int A2",
-          "options": [
-            [
-              "10",
-              "10"
+              "pose_hand_heart",
+              "pose_hand_heart"
             ],
             [
-              "20",
-              "20"
+              "present_double",
+              "present_double"
+            ],
+            [
+              "wave_two_hand_sway",
+              "wave_two_hand_sway"
+            ],
+            [
+              "head_left_down",
+              "head_left_down"
+            ],
+            [
+              "head_right_down",
+              "head_right_down"
+            ],
+            [
+              "head_left_up",
+              "head_left_up"
+            ],
+            [
+              "head_left_down",
+              "head_left_down"
+            ],
+            [
+              "double_nod",
+              "double_nod"
+            ],
+            [
+              "highfive_ready",
+              "highfive_ready"
+            ],
+            [
+              "highfive_strike",
+              "highfive_strike"
+            ],
+            [
+              "fistbump_ready",
+              "fistbump_ready"
+            ],
+            [
+              "fistbump_strike",
+              "fistbump_strike"
             ]
           ],
-          "default": "10",
-          "option_parameters": {}
+          "default": "wave_two_hand",
+          "option_parameters": {},
+          "option_descriptions": {
+            "wave_two_hand": "양손 가슴높이 손인사",
+            "pose_hand_heart": "양손 손하트 만들기-- 하트 상태로 남아있을 듯... 복귀 확인 필요!!",
+            "present_double": "양손 1초 시간 차이로 손바닥이 위로 하게 드는 모션",
+            "wave_two_hand_sway": "양팔 손인사 (좌우로 허리 움직이며) 후 손 내리기  // 정중앙 기준 +- 30도 내외 // 부드럽게 바꿈",
+            "head_left_down": "머리 오른쪽 위 이동 후 복귀",
+            "head_right_down": "머리 오른쪽 아래 이동 후 복귀",
+            "head_left_up": "머리 왼쪽 위 이동 후 복귀",
+            "double_nod": "머리 두번 끄덕끄덕",
+            "highfive_ready": "하이파이브1 : 하이파이브를 하기 위해 오른손을 펴서 얼굴 높이까지 올림",
+            "highfive_strike": "하이파이브2 (하이파이브1 이후에 가능) : 오른손을 마주치는 동작 (끝나고 손을 내리는지 확인 필요!!)",
+            "fistbump_ready": "주먹인사1 : 주먹인사를 하기 위해 오른손을 주먹져서 얼굴 높이까지 올림",
+            "fistbump_strike": "주먹인사2 (주먹인사1 이후에 가능) : 오른 주먹을 마주치는 동작 (끝나고 손을 내리는지 확인 필요!!)"
+          }
         }
       ],
-      "exp_motion": [
+      "Pose": [
         {
-          "name": "B1",
-          "field": "OPT_STATE_B1",
+          "name": "name",
+          "field": "OPT_TASK_TYPE_NAME",
           "type": "string",
-          "description": "String B1",
-          "options": [],
-          "default": "",
-          "option_parameters": {}
+          "description": "Motion name",
+          "options": [
+            [
+              "work_ready",
+              "work_ready"
+            ],
+            [
+              "move",
+              "move"
+            ]
+          ],
+          "default": "work_ready",
+          "option_parameters": {},
+          "option_descriptions": {
+            "work_ready": "팔꿈치 90도 앞으로 양팔 앞으로 나란히",
+            "move": "1차 주행 모션 (살짝 팔을 뒤로 보내며 팔꿈치 접은 상태)"
+          }
+        }
+      ],
+      "Manipulation": [
+        {
+          "name": "name",
+          "field": "OPT_TASK_TYPE_NAME",
+          "type": "string",
+          "description": "Motion name",
+          "options": [
+            [
+              "prepare",
+              "prepare"
+            ],
+            [
+              "pick_up",
+              "pick_up"
+            ],
+            [
+              "dump",
+              "dump"
+            ],
+            [
+              "put_down",
+              "put_down"
+            ]
+          ],
+          "default": "prepare",
+          "option_parameters": {},
+          "option_descriptions": {
+            "prepare": "demo2604_pick_box_ready",
+            "pick_up": "demo2604_pick_box",
+            "dump": "demo2604_dump_box",
+            "put_down": "demo2604_place_box"
+          }
         },
         {
-          "name": "B2",
-          "field": "OPT_STATE_B2",
-          "type": "integer",
-          "description": "Int B2",
-          "options": [],
-          "default": "0",
-          "option_parameters": {}
+          "name": "objects",
+          "field": "OPT_TASK_TYPE_OBJECTS",
+          "type": "string",
+          "description": "objects (string)",
+          "options": [
+            [
+              "box",
+              "box"
+            ]
+          ],
+          "default": "box",
+          "option_parameters": {},
+          "option_descriptions": {
+            "box": ""
+          }
         }
       ]
     }
@@ -74,8 +169,7 @@ function collectOptionParams(block, defs, out) { (defs || []).forEach((meta) => 
 
 javascriptGenerator.forBlock['behavior__motion__motion_start_motion'] = function(block, generator) {
   const parameter = {};
-  parameter['name'] = block.getFieldValue('PARAM_NAME') || '';
-  parameter['state'] = block.getFieldValue('PARAM_STATE') || '';
+  parameter['task_type'] = block.getFieldValue('PARAM_TASK_TYPE') || '';
   parameter['repeat'] = block.getFieldValue('PARAM_REPEAT') || '';
   parameter['action'] = block.getFieldValue('PARAM_ACTION') || '';
   const optionMetaByField = OPTION_PARAM_MAP['behavior__motion__motion_start_motion'] || {};
