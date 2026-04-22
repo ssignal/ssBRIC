6b5abf1d8b8a17117302c2c9f7f9a9e0f5f40a3e
kiho.choi <kiho.choi@lge.com>
2026-04-22 09:46:07 +0900

    [ssBRIC] Remove BRIC to upgrade

--- BRIC/app/static/generated/CustomBlocks/index.js
+++
@@ -1,4 +0,0 @@
-window.BRIC = window.BRIC || {};
-window.BRIC.registerBlocksAll = function() {
-  (window.BRIC.blockRegistrars || []).forEach((fn) => fn());
-};
--- BRIC/genBehaviorList/runGen.sh
+++
@@ -1,3 +0,0 @@
-cd ../
-python3 ./generate_block.py --repo-root . --prompt prompt_block_btList
-cd -
